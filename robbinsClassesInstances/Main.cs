d9264024909b78ae067f3d57d7c5f6a2678eaf8c
a-robbins <45301147+a-robbins@users.noreply.github.com>
2021-01-30 19:28:35 -0800

    stage 1 of rectangle and square implemented

--- robbinsClassesInstances/Main.cs
+++ robbinsClassesInstances/Main.cs
@@ -9,9 +9,15 @@ namespace robbinsClassesInstances
         public static void Main(String[] args)
         {
             var square1 = new Square(10);
-            var area = square1.calcArea(10);
+            var rectangle1 = new Rectangle(5, 4);
 
-            Console.WriteLine("The area of square1 is: " + area);
+            Console.WriteLine(); 
+
+            Console.WriteLine("The area of square1 is: " + square1.calcArea(10));
+            Console.WriteLine("The perimeter of square1 is: " + square1.calcPerim(10));
+            Console.WriteLine("The area of rectangle1 is: " + rectangle1.calcArea(5, 4));
+            Console.WriteLine("The perimeter of rectangle1 is: " + rectangle1.calcPerim(5, 4));
+
         }
     }
 }
---
+++ robbinsClassesInstances/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace robbinsClassesInstances
+{
+    class Rectangle
+    {
+        private double l;
+        private double w;
+
+        public Rectangle(double l, double w)
+        {
+            Console.WriteLine("initializing new Rectangle with length of: " + l + " and width of: " + w);
+        }
+
+        public double calcArea(double l, double w)
+        {
+            return (l * w);
+        }
+
+        public double calcPerim(double l, double w)
+        { 
+            
+            return ((2*l) + (2*w));
+        }
+
+    }
+}
